0c43d2f71f8cdd5b8775e3d5e480e963b33249ee
WooSooWP <julekwitkowski3c@gmail.com>
2026-01-13 12:52:32 +0100

    Initial commit – ReactAiFront

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import LoginPage from './Login/LoginPage';
-import Dashboard from './Dashboard/Dashboard';
-import UserStatsPage from './Stats/UserStatsPage';
-import CompanyStatsPage from './Stats/CompanyStatsPage';
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./Login/LoginPage";
+import Dashboard from "./Dashboard/Dashboard";
+import UserStatsPage from "./Stats/UserStatsPage";
+import CompanyStatsPage from "./Stats/CompanyStatsPage";
 
 const App: React.FC = () => {
-    return (
-        <Routes>
-            {/* start -> login */}
-            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
 
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/stats" element={<UserStatsPage />} />
-            <Route path="/stats/company" element={<CompanyStatsPage />} />
-        </Routes>
-    );
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/stats" element={<UserStatsPage />} />
+      <Route path="/stats/company" element={<CompanyStatsPage />} />
+
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  );
 };
 
 export default App;
